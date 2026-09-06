a6046d6ce579b6c64aa7dac57ed4a811a8cb99dd
sanju <sanjukalathil05@gmail.com>
2026-09-06 21:02:25 +0530

    ProductList component successfully created and listed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
+import ProductList from './components/productList/ProductList'
+import products from './data/products'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
+
+      <main className="mx-auto max-w-7xl px-6 py-10">
+        <ProductList products={products} />
+      </main>
     </>
   )
 }
--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import ProductCard from "../productCard/ProductCard";
 
-const ProductList = () => {
+const ProductList = ({ products }) => {
+
+    console.log(products);
+    
   return (
-    <div>ProductList</div>
-  )
-}
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {products.map((product) => (      //no return (  ::  return {
+        <ProductCard
+          key={product.id}
+          product={product}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default ProductList
+export default ProductList;
